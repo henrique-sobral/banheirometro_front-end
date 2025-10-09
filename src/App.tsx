961eef92f9b4919557c21266f47853b6020ba5e7
Henrique Sobral <114748247+henrique-sobral@users.noreply.github.com>
2025-10-08 21:19:16 -0300

    commit atualizacao
    Exclui partes nativas que vem do Vite para deixar o projeto pronto para o desenvolvimento

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, dignissimos nam ut quia quaerat ipsam accusamus numquam incidunt facere consectetur vero totam voluptatum nihil perspiciatis est neque soluta debitis quos.</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Banheirometrô</h1>
     </>
   )
 }
